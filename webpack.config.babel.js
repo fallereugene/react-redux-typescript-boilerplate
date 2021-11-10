f407f2b3a113fa0a986f2094336890e5f82682d6
Faller Eugene <faller.eugene@gmail.com>
2021-11-10 17:29:29 +0300

    Update manifest after node version checking

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,11 @@
 import merge from 'webpack-merge';
-import { mode, isSuitableNodeEngine } from './webpack/utils';
+import { 
+    mode, 
+    getParsedArguments, 
+    isSuitableNodeEngine, 
+    updateManifest, 
+    updateReleaseNotes 
+ } from './webpack/utils';
 import baseConfig from './webpack/base';
 import devConfig from './webpack/dev';
 import prodConfig from './webpack/prod';
@@ -7,5 +13,11 @@ import prodConfig from './webpack/prod';
 isSuitableNodeEngine();
 
 export default () => {
+    const type = getParsedArguments().type;
+    if (type) {
+        updateManifest(type);
+        updateReleaseNotes();
+    }
+    console.warn(`Running system building...`);
     return mode === `production` ? merge(baseConfig, prodConfig) : merge(baseConfig, devConfig);
 }
--- webpack/utils/is-suitable-node-engine.js
+++ webpack/utils/is-suitable-node-engine.js
@@ -3,7 +3,7 @@ import { engines } from '../../package.json';
 
 const isSuitableNodeEngine = () => {
     if (!semver.satisfies(process.version, engines.node)) {
-        console.log(`Current node version ${process.version} less than minimum required: ${engines.node}`);
+        console.log(`Not suitable node version ${process.version}. Required version is ${engines.node}`);
         process.exit(1);
     }
     return true;
